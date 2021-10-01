0e77f6d4ec3852b45d0f7648da586b084a693d5f
tosuja <petrkleema@gmail.com>
2021-09-30 23:06:48 -0400

    Support for variable number of rows to place tiles in some reasonable table layout. Expects 16/9 screen aspect ratio

---
+++ app/src/main/java/com/klima/yesno/Geometry.kt
@@ -0,0 +1,3 @@
+package com.klima.yesno
+
+data class Geometry(val rows: Int, val columns: Int)
--- app/src/main/java/com/klima/yesno/MainActivity.kt
+++ app/src/main/java/com/klima/yesno/MainActivity.kt
@@ -2,27 +2,85 @@ package com.klima.yesno
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import org.xmlpull.v1.XmlPullParser
+import kotlin.math.roundToInt
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
-        val tableRow = TableRow(this)
-        tableRow.layoutParams = TableRow.LayoutParams(
-            TableRow.LayoutParams.MATCH_PARENT,
-            TableRow.LayoutParams.MATCH_PARENT,
-            10.0F
-        )
-        tableRow.tag = "tableRow0"
-        tableLayout.addView(tableRow)
+        val tiles = parseTileResources()
 
+        val geometry: Geometry = calculateGeometry(tiles.size)
+
+        val rowIds = createTableRows(geometry)
+
+        spreadTiles(tiles, geometry, rowIds)
+    }
+
+    private fun spreadTiles(
+        tiles: ArrayList<Tile>,
+        geometry: Geometry,
+        rowIds: ArrayList<Int>
+    ) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        var column = 1
+        var row = 0
+        for (tile: Tile in tiles) {
+            if (column > geometry.columns) {
+                column = 1
+                row += 1
+            }
+            column += 1
+
+            val fragment = TextInCircle.newInstance(tile.colour, tile.text)
+            fragmentTransaction.add(rowIds[row], fragment)
+        }
+        fragmentTransaction.commit()
+    }
+
+    private fun createTableRows(geometry: Geometry): ArrayList<Int> {
+        val rowIds = ArrayList<Int>()
+        val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
+        for (rowNumber in 1..geometry.rows) {
+            val tableRow = TableRow(this)
+            tableRow.layoutParams = TableRow.LayoutParams(
+                TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.MATCH_PARENT,
+            )
+            val viewId = View.generateViewId()
+            tableRow.id = viewId
+            tableLayout.addView(tableRow,
+                TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT,
+                    1F / geometry.rows
+                ))
+            rowIds.add(viewId)
+        }
+        return rowIds
+    }
+
+    private fun calculateGeometry(numberOfTiles: Int): Geometry {
+        val screenRatio = 16F/9F
+        var rowCount = 1F
+        var columnCount = numberOfTiles
+        var nextRatio = numberOfTiles / (rowCount + 1)
+        while (screenRatio <= nextRatio) {
+            rowCount += 1
+            columnCount = (numberOfTiles / rowCount).roundToInt()
+            nextRatio = columnCount / rowCount
+        }
+        return Geometry(rowCount.toInt(), columnCount)
+    }
+
+    private fun parseTileResources(): ArrayList<Tile>
+    {
         val tiles = ArrayList<Tile>()
         val parser = resources.getXml(R.xml.tiles)
         parser.next()
@@ -31,8 +89,7 @@ class MainActivity : AppCompatActivity() {
             val name: String? = parser.name
             if (name == "tile") {
                 if (eventType == XmlPullParser.START_TAG) {
-                    val tile = Tile(tag = parser.getAttributeValue(1),
-                        colour = Color.parseColor(parser.getAttributeValue(0)),
+                    val tile = Tile(colour = Color.parseColor(parser.getAttributeValue(0)),
                         text = parser.getAttributeValue(2)
                     )
                     tiles.add(tile)
@@ -40,12 +97,6 @@ class MainActivity : AppCompatActivity() {
             }
             eventType = parser.next()
         }
-
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        for (tile: Tile in tiles) {
-            val fragment = TextInCircle.newInstance(tile.colour, tile.text)
-            fragmentTransaction.add(R.id.tableRow0, fragment, tile.tag)
-        }
-        fragmentTransaction.commit()
+        return tiles
     }
 }
